609e23c233402077539c41682f0bb15ba625968d
lokeshdhote <lokeshdhote2004@gmail.com>
2024-04-13 22:46:08 +0530

    q

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ const  mongoose = require("mongoose")
 
 const plm = require("passport-local-mongoose")
 
-mongoose.connect("mongodb://localhost:27017/herbalAuryeda")
-
+// mongoose.connect("mongodb://localhost:27017/herbalAuryeda")
+mongoose.connect("mongodb+srv://lokeshdhote2004:1IAIRPQOlkBEBofY@cluster0.kn0pnk1.mongodb.net/")
+// mongodb+srv://lokeshdhote2004:<password>@cluster0.kn0pnk1.mongodb.net/
 
 const userSchema = mongoose.Schema({
     username:String,
