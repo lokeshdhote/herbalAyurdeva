c9ba7d42be9bf250a907f06d18dce7521633148b
lokeshdhote <lokeshdhote2004@gmail.com>
2024-04-14 11:33:47 +0530

    eeeee

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const  mongoose = require("mongoose")
 
 const plm = require("passport-local-mongoose")
 
-mongoose.connect("mongodb://127.0.0.1:27017/herbalAuryeda")
+mongoose.connect("mongodb://0.0.0.0:27017/herbalAuryeda")
 
 const userSchema = mongoose.Schema({
     username:String,
